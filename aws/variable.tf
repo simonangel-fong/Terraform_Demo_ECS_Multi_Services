40d764e109ea28d29b71309971adfab729b6b44a
simonangel-fong <simonangelfong@gmail.com>
2025-11-11 21:55:08 -0500

    tf init

---
+++ aws/variable.tf
@@ -0,0 +1,43 @@
+# ##############################
+# APP
+# ##############################
+variable "project" { default = "demo-ecs-postgres" }
+
+# ##############################
+# AWS
+# ##############################
+variable "aws_region" { type = string }
+
+# ##############################
+# Cloudflare
+# ##############################
+variable "cloudflare_api_token" { type = string }
+variable "cloudflare_zone_id" { type = string }
+
+# ##############################
+# AWS VPC
+# ##############################
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_subnet" {
+  type = map(object({
+    subnet_name = string
+    cidr_block  = string
+    az_suffix   = string
+  }))
+  default = {
+    public_subnet_1a = {
+      subnet_name = "public_subnet"
+      cidr_block  = "10.0.1.0/24"
+      az_suffix   = "a"
+    }
+    public_subnet_1b = {
+      subnet_name = "public_subnet"
+      cidr_block  = "10.0.2.0/24"
+      az_suffix   = "b"
+    }
+  }
+}
