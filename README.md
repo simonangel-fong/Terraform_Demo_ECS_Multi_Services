cc57daddfa9523eb4fcfa1aa2ea750a7fb0bd7c2
simonangel-fong <simonangelfong@gmail.com>
2025-11-20 14:06:03 -0500

    k6: refactor load test

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ docker run --rm --name k6_smoke --net=app_public_network -p 5665:5665 -e BASE_UR
 
 docker run --rm --name k6_smoke --net=app_public_network -p 5665:5665 -e BASE_URL=http://nginx:8000 -e K6_WEB_DASHBOARD=true -v ./k6/script:/script -v ./k6/report:/report/ grafana/k6 run /script/test_baseline.js
 
+docker run --rm --name k6_smoke --net=app_public_network -p 5665:5665 -e BASE_URL=http://nginx:8000 -e K6_WEB_DASHBOARD=true -v ./k6/script:/script -v ./k6/report:/report/ grafana/k6 run /script/test_load.js
+
--- k6/script/test_load.js
+++ k6/script/test_load.js
@@ -1,4 +1,4 @@
-// test_baseline.js
+// test_load.js
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.4/index.js";
 import { parseNumberEnv, getDeviceForVU } from "./common.js";
 import { getTelemetryLatest, postTelemetry } from "./target_url.js";
@@ -6,53 +6,58 @@ import { getTelemetryLatest, postTelemetry } from "./target_url.js";
 // ==============================
 // Environment parameters
 // ==============================
-
 const BASE_URL = __ENV.BASE_URL || "http://localhost:8000";
 
-const DEVICE_VU = parseNumberEnv("DEVICE_VU", 100); // # of devices
+const DEVICE_VU = parseNumberEnv("DEVICE_VU", 600); // # of devices
 const DEVICE_INTERVAL = parseNumberEnv("DEVICE_INTERVAL", 10); // interval of device transmit data
 const DEVICE_RATE = Math.ceil(DEVICE_VU / DEVICE_INTERVAL); // post rate
 
-const HUB_VU = parseNumberEnv("HUB_VU", 10); // # of hub
+const HUB_VU = parseNumberEnv("HUB_VU", 60); // # of hub
 const HUB_INTERVAL = parseNumberEnv("HUB_INTERVAL", 10); // interval of hub request data
 const HUB_RATE = Math.ceil(HUB_VU / HUB_INTERVAL); // get rate
 
-const DURATION = parseNumberEnv("DURATION", 5);
+const DURATION = parseNumberEnv("DURATION", 10); // minute
 
 // ==============================
 // k6 options
 // ==============================
 export const options = {
   cloud: {
-    name: "Baseline Testing",
+    name: "Load Testing",
   },
   thresholds: {
     http_req_failed: ["rate<0.001"], // HTTP-level failures
-    http_req_duration: ["p(95)<500"], // global latency
 
-    // latency for telemetry POSTs
-    "http_req_duration{endpoint:telemetry_post}": ["p(95)<100", "p(99)<200"],
+    // post
+    "http_req_duration{endpoint:telemetry_post}": [
+      "p(50)<50", // median very fast
+      "p(95)<100", // SLO target
+      "p(99)<150", // 99
+    ],
+
+    // get
+    "http_req_duration{endpoint:telemetry_get}": ["p(95)<200", "p(99)<400"],
   },
 
   scenarios: {
-    // device
-    baseline_telemetry_post: {
+    // devices
+    load_telemetry_post: {
       executor: "constant-arrival-rate",
       preAllocatedVUs: DEVICE_VU,
-      rate: DEVICE_RATE, // writes per second
+      rate: DEVICE_RATE, // post rate
       duration: `${DURATION}m`,
       gracefulStop: "30s",
-      exec: "baseline_telemetry_post",
+      exec: "load_telemetry_post",
     },
 
-    // dashboard
-    baseline_telemetry_get_lastest: {
+    // hub dashboard
+    load_telemetry_get: {
       executor: "constant-arrival-rate",
       preAllocatedVUs: HUB_VU,
-      rate: HUB_RATE, // read per second
+      rate: HUB_RATE, // get rate
       duration: `${DURATION}m`,
-      gracefulStop: "10s",
-      exec: "baseline_telemetry_get_lastest",
+      gracefulStop: "30s",
+      exec: "load_telemetry_get",
     },
   },
 };
@@ -62,22 +67,22 @@ export const options = {
 // ==============================
 const device = getDeviceForVU();
 
-export function baseline_telemetry_post() {
+export function load_telemetry_post() {
   postTelemetry({ base_url: BASE_URL, device });
 }
 
-export function baseline_telemetry_get_lastest() {
+export function load_telemetry_get() {
   getTelemetryLatest({ base_url: BASE_URL, device });
 }
 
-export default baseline_telemetry_post;
+export default load_telemetry_post;
 
 // ==============================
 // Summary output
 // ==============================
 export function handleSummary(data) {
   return {
-    "summary_baseline.json": JSON.stringify(data, null, 2),
+    "summary_load.json": JSON.stringify(data, null, 2),
     stdout: textSummary(data, { indent: " ", enableColors: true }),
   };
 }
